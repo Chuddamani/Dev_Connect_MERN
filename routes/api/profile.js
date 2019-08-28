235af4c423be81c51723ea0112ab5e09e3f3a2e1
Chuddamani <chuddamani@cprasad.local>
2019-08-28 19:18:24 +0530

    fetching git repo details and Post Creation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -330,6 +332,37 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send('Internal Server Error');
+  }
+});
+
+// @route  DELETE api/profile/github/:username
+// @desc   Get user repso from GIthub
+// @access Public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id${config.get(
+        'gthubCliemtId'
+      )}&client_secret=${config.get('githubClientSecret')}`,
+
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode != 200) {
+        return res.status(404).json({ msg: 'No Github Profile Found' });
+      }
+      res.json(JSON.parse(body));
+    });
   } catch (error) {
     console.log(error.message);
     return res.status(500).send('Internal Server Error');
