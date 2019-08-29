6f0202c67270af6691ae9d47d1398d90817d442f
Chuddamani <chuddamani@cprasad.local>
2019-08-30 05:21:29 +0530

    Register and Login form created

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,65 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <Fragment>Login</Fragment>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Fragment>
+      <section className='container'>
+        <div className='alert alert-danger'>Invalid credentials</div>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Sign into Your Account
+        </p>
+        <form className='form' action='dashboard.html'>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              value={password}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <input
+            type='submit'
+            className='btn btn-primary'
+            value='Login'
+            onSubmit={e => onSubmit(e)}
+          />
+        </form>
+        <p className='my-1'>
+          Don't have an account? <Link to='/register'>Sign Up</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Login;
