7f0c87bc7a50e23f6c6c24dfb29ea9296a265019
Chuddamani <chuddamani@cprasad.local>
2019-09-04 18:04:05 +0530

    Got user profile into redux state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 export default combineReducers({
   // We will add reduces here
   alert: alert,
-  auth: auth
+  auth: auth,
+  profile: profile
 });
