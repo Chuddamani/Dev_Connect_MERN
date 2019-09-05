6aba09c253ed864bca35691f868403a681602175
Chuddamani <chuddamani@cprasad.local>
2019-09-05 18:48:39 +0530

    Profile educationa and experience creation done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_EROR } from './types';
+import { GET_PROFILE, UPDATE_PROFILE, PROFILE_EROR } from './types';
 
 // Get the current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -59,5 +59,53 @@ export const createProfile = (
         status: error.response.status
       }
     });
+  }
+};
+
+//Add Experience
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    const res = await axios.put('/api/profile/experience', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert('Experience Added', 'success'));
+    // we cant use <Redirect> in action , we can use that in component only
+    history.push('/dashboard');
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+  }
+};
+
+//Add Education
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    const res = await axios.put('/api/profile/education ', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert('Education Added', 'success'));
+    // we cant use <Redirect> in action , we can use that in component only
+    history.push('/dashboard');
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
   }
 };
