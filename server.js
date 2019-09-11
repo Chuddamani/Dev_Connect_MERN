75db9827c6b1125953bd21fcb74723f5b4a69a7e
Chuddamani <chuddamani@cprasad.local>
2019-09-11 20:30:02 +0530

    Changes done to deploy in Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // Got the refrence to the  exported module in db.js
 const connectDB = require('./config/db');
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000; //No port defined it would go to 5000
 app.listen(port, () => console.log(`Server Started on PORT ${port}`));
 
 // Just to check is server is up and running
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 
 //Init Middleware to handle json request
 app.use(express.json({ extended: false }));
@@ -20,3 +21,12 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/post', require('./routes/api/post'));
+
+// Server Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set Static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
